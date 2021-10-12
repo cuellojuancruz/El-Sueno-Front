2e548a7b04e8d02c1f3ffb9a91935281deae4c1f
cuellojuancruz <cuellojuancruz11@gmail.com>
2021-10-12 10:52:25 -0300

    creacion del Login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import NavBar  from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 
 
 import React from "react";
 import { Route } from "react-router-dom";
 import './App.css';
+import LogOut from "./components/LogOut/LogOut";
 
 function App() {
   return (
     <React.Fragment>
+      <Route exact path="/" component={Login} />
+      <Route path="/home" component={LogOut} />    
       <Route path="/home" component={NavBar} />
       <Route exact path="/home" component={Home} />
     </React.Fragment>
--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/index';
+import  { Auth0Provider } from "@auth0/auth0-react"
 
 ReactDOM.render(
   <Provider store={store}>
     {/* <React.StrictMode> */}
       <BrowserRouter>
-          <App />
+        <Auth0Provider domain="dev-4ivkxcfj.us.auth0.com" clientId="F1atauA05Hr2iD9NveSQuRk9sI0ELQjg" redirectUri="http://localhost:3000/home">
+          <App/>
+        </Auth0Provider>
       </BrowserRouter>
     {/* </React.StrictMode> */}
   </Provider>,
